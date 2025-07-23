bcff0e1ea1dd3f5eba86c626afe76fdc86ada1d4
KevinSoto07 <kevingranillo28@gmail.com>
2025-07-22 21:15:57 -0600

    final changes

--- view/Tecnología/js/tecnología.js
+++ view/Tecnología/js/tecnología.js
@@ -1,4 +1,4 @@
-// aside menu
+// aside menu en dispositivos moviles
 const btn = document.getElementById('hamburger-btn');
 const closeBtn = document.getElementById('close-btn');
 const aside = document.getElementById('side-menu');
@@ -13,3 +13,20 @@ function closeAsideMenu(){
 
 btn.addEventListener('click',openAsideMenu);
 closeBtn.addEventListener('click',closeAsideMenu);
+
+// interacción botones de las cards
+const cardButtons = document.querySelectorAll('.button');
+cardButtons.forEach(button =>{
+    button.addEventListener('click',() =>{
+        button.style.backgroundColor = '#ff9900';
+        button.textContent = 'Comprado';
+    })
+});
+
+// cambio de color logo "FlashBuy"
+const flashBuyLogo = document.getElementById('page_name');
+if (flashBuyLogo){
+    flashBuyLogo.addEventListener('click',()=>{
+        flashBuyLogo.style.color = '#232f3e'
+    })
+}
